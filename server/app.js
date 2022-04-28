8b3d0352215a573272d7f0308725998fa8c8c552
Jonne Jormakka <itjonne@gmail.com>
2022-04-28 16:09:30 +0300

    laitettu oikea static-serve kansio

--- server/app.js
+++ server/app.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const helmet = require("helmet");
-const path = require("path");
-require("express-async-errors");
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
+const path = require('path');
+require('express-async-errors');
 
-const config = require("./util/config");
-const middleware = require("./util/middleware");
-const usersRouter = require("./controllers/users");
-const objectsRouter = require("./controllers/objects");
-const authRouter = require("./controllers/auth");
-const findsRouter = require("./controllers/finds");
+const config = require('./util/config');
+const middleware = require('./util/middleware');
+const usersRouter = require('./controllers/users');
+const objectsRouter = require('./controllers/objects');
+const authRouter = require('./controllers/auth');
+const findsRouter = require('./controllers/finds');
 
 mongoose
   .connect(config.MONGODB_URI, {
@@ -19,34 +19,34 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB');
   })
   .catch((error) => {
-    console.error("error connection to MongoDB:", error);
+    console.error('error connection to MongoDB:', error);
   });
 
 const app = express();
 app.use(express.json());
-app.use(morgan("common"));
+app.use(morgan('common'));
 app.use(cors());
 app.use(helmet());
 
 // ... other app.use middleware
-app.use(express.static(path.join(__dirname, "frontend", "build")));
-
-// ...
-// Right before your app.listen(), add this:
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
-});
+app.use(express.static(path.join(__dirname, '../frontend', 'build')));
 
 app.use(middleware.tokenExtractor);
 app.use(middleware.userExtractor);
 
-app.use("/api/users", usersRouter);
-app.use("/api/objects", objectsRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/finds", findsRouter);
+// ...
+// Right before your app.listen(), add this:
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
+});
+
+app.use('/api/users', usersRouter);
+app.use('/api/objects', objectsRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/finds', findsRouter);
 
 app.use(middleware.errorHandler);
 app.use(middleware.unknownEndpoint);
